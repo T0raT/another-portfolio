386efea7b316ee60c290458b2e259c101dfdc269
T0ra <tigerliu@bu.edu>
2024-08-15 17:19:15 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <header className="animate-intro-shrink"></header>
+      <main className="bg-[#690026] animate-intro-grow"></main>
+      <footer className="animate-intro-shrink"></footer>
+    </div>
+  );
+}
