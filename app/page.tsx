6e0bbb8b899b56f1e57a0beacd622edb99379101
T0ra <tigerliu@bu.edu>
2024-08-22 02:21:51 -0400

    Completely re-modeled layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,70 @@
+"use client";
+import { useState } from "react";
 import { Noto_Serif_Display } from "next/font/google";
-
+import "../ui/static.css";
+import "../ui/text-effects.css";
 const notoSansJP = Noto_Serif_Display({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
 
 export default function Home() {
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+  const [navHover, setNavHover] = useState<boolean>(false);
+
+  const handleMouseEnter = (item: string) => {
+    setHoveredItem(item);
+  };
+
   return (
     <div className="w-screen h-screen flex flex-col">
-      <header className="z-100 animate-intro-shrink"></header>
+      <header className="flex z-100 animate-intro-shrink bg-[#3c3836]">
+        {/* Can use a loop to render this later I think */}
+        <nav
+          onMouseEnter={() => setNavHover(true)}
+          onMouseLeave={() => setNavHover(false)}
+          className="self-end flex justify-around py-4 tracking-in-expand-slow"
+        >
+          <button
+            className="text-gruv-fg0 text-xl ml-5 "
+            onMouseEnter={() => handleMouseEnter("About Me")}
+          >
+            About Me
+          </button>
+          <button
+            className="text-gruv-fg0 text-xl ml-5"
+            onMouseEnter={() => handleMouseEnter("Projects")}
+          >
+            Projects
+          </button>
+          <button
+            className="text-gruv-fg0 text-xl ml-5"
+            onMouseEnter={() => handleMouseEnter("Contact Me")}
+          >
+            Contact Me
+          </button>
+        </nav>
+      </header>
       <main
-        className={`${notoSansJP.className} z-1 overflow-y-auto bg-[#940707] h-full flex flex-col justify-center`}
+        className={`${notoSansJP.className} bg-gruv-bg0-h z-50 overflow-clip relative h-full flex flex-col justify-center`}
       >
-        <h1 className="m-auto my-0 text-5xl font-bold">Tiger Liu</h1>
-        <h3 className="m-auto my-0 mt-20 text-xl">
-          React Nextjs CSS TailwindCSS Python C
-        </h3>
+        {navHover && (
+          <div className="absolute flex flex-col justify-center items-center w-full h-full z-30 backdrop-blur-lg text-5xl font-bold text-gruv-fg0">
+            <h1 className="tracking-in-expand">{hoveredItem}</h1>
+          </div>
+        )}
+        <h1 className="z-10 m-auto my-0 gap-10 text-center text-5xl font-bold text-gruv-fg0 ">
+          Tiger Liu
+        </h1>
+        <ul className=" my-0 mt-10 text-xl self-center text-gruv-fg0 text-flicker-in-glow">
+          <li className="inline-block">React</li>
+          <li className="inline-block ml-5">NextJS</li>
+          <li className="inline-block ml-5">CSS</li>
+          <li className="inline-block ml-5">Tailwind CSS</li>
+          <li className="inline-block ml-5">ThreeJS</li>
+        </ul>
       </main>
-      <footer className="z-100 animate-intro-shrink"></footer>
+      <footer className="z-100 animate-intro-shrink bg-[#3c3836]"></footer>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        gruv: {
+          "bg0-h": "#1d2021",
+          bg: "#282828",
+          bg0: "#282828",
+          bg1: "#3c3836",
+          fg0: "#fbf1c7",
+          fg1: "#ebdbb2",
+          gray: "#a89984",
+          red: "cc241d",
+          green: "#98971a",
+          yellow: "#d79921",
+        },
+      },
     },
   },
   plugins: [],
