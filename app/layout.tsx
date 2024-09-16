db489ff292a25703933f432752c6b0e8e4f7f62b
T0ra <tigerliu@bu.edu>
2024-09-16 05:42:57 -0400

    CHanged site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ballet Labyrinth Night",
+  title: "tigerliu.cc",
   description: "Ballet Labyrinth Night",
 };
 
